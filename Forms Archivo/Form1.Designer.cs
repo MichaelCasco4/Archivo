d7c95d05bcfb630a90a57962ad34c7f0e95e55fb
MichaelCasco4 <mecascov@uamv.edu.ni>
2024-10-04 08:46:53 -0600

    Code created on .NET FORMS

---
+++ Forms Archivo/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Forms_Archivo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dgvPersonas = new System.Windows.Forms.DataGridView();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbYears = new System.Windows.Forms.TextBox();
+            this.tbGrade = new System.Windows.Forms.TextBox();
+            this.tbGender = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersonas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First and Last Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Years old";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Final Grade";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(317, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Gender";
+            // 
+            // dgvPersonas
+            // 
+            this.dgvPersonas.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dgvPersonas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPersonas.Location = new System.Drawing.Point(12, 280);
+            this.dgvPersonas.Name = "dgvPersonas";
+            this.dgvPersonas.Size = new System.Drawing.Size(399, 158);
+            this.dgvPersonas.TabIndex = 4;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(12, 78);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(164, 20);
+            this.tbName.TabIndex = 5;
+            // 
+            // tbYears
+            // 
+            this.tbYears.Location = new System.Drawing.Point(12, 148);
+            this.tbYears.Name = "tbYears";
+            this.tbYears.Size = new System.Drawing.Size(164, 20);
+            this.tbYears.TabIndex = 6;
+            // 
+            // tbGrade
+            // 
+            this.tbGrade.Location = new System.Drawing.Point(12, 226);
+            this.tbGrade.Name = "tbGrade";
+            this.tbGrade.Size = new System.Drawing.Size(164, 20);
+            this.tbGrade.TabIndex = 7;
+            // 
+            // tbGender
+            // 
+            this.tbGender.Location = new System.Drawing.Point(254, 78);
+            this.tbGender.Name = "tbGender";
+            this.tbGender.Size = new System.Drawing.Size(164, 20);
+            this.tbGender.TabIndex = 8;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(284, 123);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(95, 45);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add Results";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(284, 179);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(95, 45);
+            this.btnCargar.TabIndex = 10;
+            this.btnCargar.Text = "Show Data";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(275, 254);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(127, 20);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear Code";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbGender);
+            this.Controls.Add(this.tbGrade);
+            this.Controls.Add(this.tbYears);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.dgvPersonas);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Information Register";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersonas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgvPersonas;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbYears;
+        private System.Windows.Forms.TextBox tbGrade;
+        private System.Windows.Forms.TextBox tbGender;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Forms Archivo/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms_Archivo
+{
+    struct Persona
+    {
+        public string name { get; set; }
+        public int year_old { get; set; }
+        public int grade {  get; set; }
+        public char gender { get; set; }
+
+    }
+    public partial class Form1 : Form
+    {
+        List<Persona> lista;
+        public Form1()
+        {
+            InitializeComponent();
+            lista = new List<Persona>();
+
+            dgvPersonas.Columns.Add("name", "Nombre");
+            dgvPersonas.Columns.Add("year_old", "Edad");
+            dgvPersonas.Columns.Add("grade", "Nota");
+            dgvPersonas.Columns.Add("gender", "Género");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (FileStream mArchivoEscritor = new FileStream("datos.dat", FileMode.Append, FileAccess.Write))
+            using (BinaryWriter escritor = new BinaryWriter(mArchivoEscritor))
+            {
+                try
+                {
+                    string nombre = tbName.Text;
+                    if (string.IsNullOrEmpty(nombre))
+                    {
+                        MessageBox.Show("El nombre no puede estar vacío.");
+                        return;
+                    }
+
+                    if (!int.TryParse(tbYears.Text, out int edad))
+                    {
+                        MessageBox.Show("La edad debe ser un número entero.");
+                        return;
+                    }
+
+                    if (!int.TryParse(tbGrade.Text, out int nota))
+                    {
+                        MessageBox.Show("La nota debe ser un número entero.");
+                        return;
+                    }
+
+                    if (tbGender.Text.Length != 1)
+                    {
+                        MessageBox.Show("El género debe ser un solo carácter.");
+                        return;
+                    }
+                    char genero = char.Parse(tbGender.Text);
+
+
+                    escritor.Write(nombre.Length); // Longitud del nombre
+                    escritor.Write(nombre.ToCharArray()); // Nombre
+                    escritor.Write(edad);
+                    escritor.Write(nota);
+                    escritor.Write(genero);
+
+                    MessageBox.Show("Datos guardados correctamente.");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar los datos: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            dgvPersonas.Rows.Clear();
+            lista.Clear(); // Limpia la lista antes de cargar nuevos datos
+
+            if (!File.Exists("datos.dat"))
+            {
+                MessageBox.Show("El archivo no existe.");
+                return;
+            }
+
+            using (FileStream mArchivoLector = new FileStream("datos.dat", FileMode.Open, FileAccess.Read))
+            using (BinaryReader Lector = new BinaryReader(mArchivoLector))
+            {
+                try
+                {
+                    while (mArchivoLector.Position != mArchivoLector.Length)
+                    {
+                        Persona persona = new Persona();
+                        int length = Lector.ReadInt32(); // Longitud del nombre
+                        char[] nombreArray = Lector.ReadChars(length); // Nombre
+                       persona.name = new string(nombreArray);
+                        persona.name = new string(nombreArray);
+                        persona.year_old = Lector.ReadInt32();
+                        persona.grade = Lector.ReadInt32();
+                        persona.gender = Lector.ReadChar();
+
+                        lista.Add(persona);
+                        dgvPersonas.Rows.Add(persona.name, persona.year_old, persona.grade, persona.gender);
+                    
+                
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al cargar los datos: " + ex.Message);
+                }
+            }
+
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dgvPersonas.Rows.Clear();
+
+            // Limpiar el archivo
+            if (File.Exists("datos.dat"))
+            {
+                File.Delete("datos.dat"); // Eliminar el archivo
+                MessageBox.Show("Datos borrados correctamente.");
+            }
+            else
+            {
+                MessageBox.Show("No hay datos para borrar.");
+            }
+        }
+    }
+}
